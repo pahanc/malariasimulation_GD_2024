73b62d42a60766dcbf3c2b8c6342dbd0f07b0d60
Penny Hancock <pahanc@dide.local>
2024-10-16 16:43:37 +0100

    updates to parameters.R2

--- R/parameters.R
+++ R/parameters.R
@@ -231,7 +231,7 @@ get_parameters <- function(overrides = list(),square_number=square_number, supp_
   
   #An. gambiae/coluzzii
   
-  supp_filename_gamb<-paste("W:\\Penny\\for_hpc\\Seas and supp species specific/With funestus suppression/May 24 Sourou Suppress/",supp_gam_filename,sep="")
+  supp_filename_gamb<-paste("W:\\Penny\\for_hpc\\Seas and supp species specific/With funestus suppression/Houet suppression files/",supp_gam_filename,sep="")
   mosq_suppression_gamb<-unlist(read.csv(supp_filename_gamb,header=F,colClasses="numeric"))
   dimnames(mosq_suppression_gamb)<-NULL
   if (supp_gam){
@@ -242,7 +242,7 @@ get_parameters <- function(overrides = list(),square_number=square_number, supp_
   }
   
   #An. arabiensis
-  supp_filename_arab<-paste("W:\\Penny\\for_hpc\\Seas and supp species specific/With funestus suppression/May 24 Sourou Suppress/",supp_arab_filename,sep="")
+  supp_filename_arab<-paste("W:\\Penny\\for_hpc\\Seas and supp species specific/With funestus suppression/Houet suppression files/",supp_arab_filename,sep="")
   mosq_suppression_arab<-unlist(read.csv(supp_filename_arab,header=F,colClasses="numeric"))
   dimnames(mosq_suppression_arab)<-NULL
   if (supp_arab){
@@ -253,7 +253,7 @@ get_parameters <- function(overrides = list(),square_number=square_number, supp_
   }
   
   #An. funestus
-  #supp_filename_fun<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Mar 24 Sourou/Mar 24 Sourou Suppress/",supp_fun_file,sep="")
+  #supp_filename_fun<-paste("Q:\\for_hpc\\Seas and supp species specific/With funestus suppression/Mar 24 Sourou/Houet suppression files/",supp_fun_file,sep="")
   #mosq_suppression_fun<-unlist(read.csv(supp_filename_fun,header=F,colClasses="numeric"))
   #dimnames(mosq_suppression_fun)<-NULL
   #if (supp_fun){
